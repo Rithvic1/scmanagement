2f346a9b5bd7b799e863ad992d508a0f566c2dbd
Rithvic1 <marneni.raj@s.amity.edu>
2025-02-27 11:15:20 +0530

    added multiplication line

--- calc.c
+++ calc.c
@@ -4,6 +4,7 @@ int main()
 	int a=1,b=2;
 	printf("sum of two numbers is %d,%d",&a,&b);
 	printf("difference of two numbers is %d,%d",&a,&b);
+	printf("product of two numbers is %d,%d",&a,&b);
 	return 0;
 
 }
